616c99b4d969a8fc175906ddeee994d1abaf3964
Rohankeshav <73099849+Rohankeshav@users.noreply.github.com>
2022-07-19 01:11:22 +0530

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Options</title>
+<link rel="stylesheet" href="navigation.css">
+</head>
+<body>
+    
+<a href="login.php"><h3>Logout</h3></a>   
+
+<div class="container">
+<div class="TextBox-1">
+<a href="AddCrime.php"><h1>Create</h1></a>
+</div>
+<div class="TextBox-2">
+<a href="delete2.php"><h1>Delete</h1></a>
+</div>
+<div class="TextBox-3">
+<a href="update.php"><h1>Update</h1></a>
+      </div>
+      <div class="TextBox-4">
+<a href="retrieve_data.php"><h1>Search</h1></a>
+    </div>
+</div>
+</body>
+</html>
